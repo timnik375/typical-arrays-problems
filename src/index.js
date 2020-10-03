ff70bd3aa92ca61d3df899db308ee5a1b33623d5
Mikita Tsiamshenka <timoshenkonikita@gmail.com>
2020-10-03 09:53:37 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (mn) {
+  if (mn === undefined || mn.length === 0) return 0;
+  mn.sort(function (a, b) {
+		return a - b;
+	});
+  return mn[0];   
+};
+
+exports.max = function max (mx) {
+  if (mx === undefined || mx.length === 0) return 0;
+  mx.sort(function (a, b) {
+		return b - a;
+	});
+	return mx[0]; 
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (ag) {
+  if (ag === undefined || ag.length === 0) return 0;
+  if (ag.length == 0) return 0;
+  return ag.reduce((a, b) => (a + b)) / ag.length;
 }
